3a764bf3c0037941a4b2d493743f71539d528dc0
n4ryn <vinaykumar.cs19@aryacollege.in>
2022-08-19 23:21:07 +0530

    FrontEnd and Routing modified

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,86 @@
-import { useEffect } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import { useState, useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
+import { accessToken, logout, getCurrentUserProfile } from "./spotify";
+import { catchErrors } from "./utils";
+
+// Importing Styles
+import styled from "styled-components/macro";
+import { GlobalStyle } from "./styles";
+
+// Importing Pages
+import { Login, Profile } from "./pages";
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
 
 function App() {
+  const [token, setToken] = useState(null);
+  const [profile, setProfile] = useState(null);
+
   useEffect(() => {
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
-    const accessToken = urlParams.get("access_token");
-    const refreshToken = urlParams.get("refresh_token");
+    setToken(accessToken);
 
-    console.log(accessToken);
-    console.log(refreshToken);
+    const fetchData = async () => {
+      const { data } = await getCurrentUserProfile();
+      setProfile(data);
+    };
 
-    if (refreshToken) {
-      fetch(`/refresh_token=${refreshToken}`)
-        .then((res) => res.json())
-        .then((data) => console.log(data))
-        .then((err) => console.log(err));
-    }
-  });
+    catchErrors(fetchData());
+  }, []);
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="http://localhost:8888/login">
-          Log in to Spotify
-        </a>
+        <GlobalStyle />
+        {!token ? (
+          <Login />
+        ) : (
+          <>
+            <StyledLogoutButton onClick={logout}>Log Out</StyledLogoutButton>
+
+            <Router>
+              <ScrollToTop />
+              <Routes>
+                <Route path="/top-artists" element={<h1>Top Artists</h1>} />
+                <Route path="/top-tracks" element={<h1>Top Tracks</h1>} />
+                <Route path="/playlists/:id" element={<h1>Playlist</h1>} />
+                <Route path="/playlists" element={<h1>Playlists</h1>} />
+                <Route path="/" element={<Profile />} />
+              </Routes>
+            </Router>
+          </>
+        )}
       </header>
     </div>
   );
 }
+
+const StyledLogoutButton = styled.button`
+  position: absolute;
+  top: var(--spacing-sm);
+  right: var(--spacing-md);
+  padding: var(--spacing-xs) var(--spacing-sm);
+  background-color: rgba(0, 0, 0, 0.7);
+  color: var(--white);
+  font-size: var(--fz-sm);
+  font-weight: 700;
+  border-radius: var(--border-radius-pill);
+  z-index: 10;
+  @media (min-width: 768px) {
+    right: var(--spacing-lg);
+  }
+`;
 
 export default App;
